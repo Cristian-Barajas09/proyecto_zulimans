86a07f1a5a0d2668ed385153ef7aa28995f926d2
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2023-06-21 22:04:59 -0400

    quiero dormir jhon no voy a ponerme ha acomodar los formularios

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+const btn_navbar = document.getElementById('btn-navbar')
+
+const user = ()=>{
+	window.location.href="/profile"
+}
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
